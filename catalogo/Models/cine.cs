a71c3f4e439bc55736738ce1f162c6256209f778
oscargalaviz <71297928+oscargalaviz@users.noreply.github.com>
2020-11-25 21:33:57 -0800

    qwe

---
+++ catalogo/Models/cine.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace catalogo.Models
+{
+    public class cine
+    {
+        public int id { get; set; }
+
+        public string sala { get; set; }
+
+        public string hora { get; set; }
+    }
+}
